98ac8b2b9fa3d4737217af34ce3b32b8b090b961
Kourva <118578799+Kourva@users.noreply.github.com>
2026-05-25 16:03:15 +0330

    Create ParsCalendarTime.vue

---
+++ src/components/ParsCalendarTime.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="time-container" :key="currentTime">
+        {{ currentTime }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const currentTime = ref(null)
+
+function updateTime(){
+    const now = new Date()
+    currentTime.value = new Intl.DateTimeFormat("fa-IR", {
+        calendar: "persian",
+        timeZone: "Asia/Tehran",
+        timeStyle: "full",
+        dateStyle: "full"
+    }).format(now)
+}
+
+setInterval(updateTime, 1000)
+</script>
+
+<style scoped>
+.time-container {
+    position: relative;
+}
+</style>
